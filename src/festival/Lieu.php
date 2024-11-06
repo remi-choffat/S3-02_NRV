96862c4fa4a0ede2312f6da9869711d4405163be
Segard Mathis <mathis.segard6@etu.univ-lorraine.fr>
2024-11-06 17:09:39 +0100

    ajout class Spectacle et Lieu

--- src/festival/Lieu.php
+++ src/festival/Lieu.php
@@ -1,8 +1,85 @@
 <?php
-
+declare(strict_types=1);
 namespace iutnc\nrv\festival;
+class Lieu{
 
-class Lieu
-{
+    private string $nom;
+    private string $addresse;
+    private array $image;
+    private int $nb_places_assises;
+    private int $nb_places_debout;
 
+    /**
+     * @param string $nom
+     * @param string $addresse
+     * @param int $nb_places_assises
+     * @param int $nb_places_debout
+     *crée un Lieu
+     */
+
+    public function __construct(string $nom,string $addresse,int $nb_places_assises,int $nb_places_debout)
+    {
+        $this->image= [];
+        $this->nom=$nom;
+        $this->addresse=$addresse;
+        $this->nb_places_debout = $nb_places_debout;
+        $this->nb_places_assises=$nb_places_assises;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param string $lien
+     * @return void
+     */
+    public function addImage(string $lien){
+        $this->image[] = $lien;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddresse(): string
+    {
+        return $this->addresse;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNbPlacesDebout(): int
+    {
+        return $this->nb_places_debout;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNbPlacesAssises(): int
+    {
+        return $this->nb_places_assises;
+    }
+
+    /**
+     * @return array les images du lieu
+     */
+    public function getImage(): array
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param Lieu $l
+     * @return bool vrai si les deux Lieux sont identiques
+     */
+    public function equals(Lieu $l):bool{
+        return $this->nom === $l->getNom() && $this->addresse ===$l->getAddresse();
+
+    }
 }
--- src/festival/Spectacle.php
+++ src/festival/Spectacle.php
@@ -4,5 +4,72 @@ namespace iutnc\nrv\festival;
 
 class Spectacle
 {
+private string $titre;
+private array $artites;
+private array $images;
+private string $url;
+private string $horaire;
+public function __construct($titre,$artites,$horaire)
+{
+$this->images=[];
+$this->artites=$artites;
+$this->titre=$titre;
+$this->url="";
+$this->horaire=$horaire;
+}
+
+    /**
+     * @param Soiree $s
+     * @return string génére une description du spectacle
+     */
+public function genererDescription(Soiree $s):string{
+    $res = "Le $this->titre sera réalisé par les artistes: ";
+    for ($i=0;$i<sizeof($this->artites)-2;$i++){
+        $res.=$this->artites[$i].", ";
+    }
+    $date=$s->getDate();
+    $res.="et ".$this->artites[sizeof($this->artites)-1]." aura lieu le $date à $this->horaire";
+    return $res;
+}
+
+    /**
+     * @return string
+     */
+    public function getTitre(): string
+    {
+        return $this->titre;
+    }
+
+    /**
+     * @return array
+     */
+    public function getArtites(): array
+    {
+        return $this->artites;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHoraire(): string
+    {
+        return $this->horaire;
+    }
+
+    /**
+     * @return array
+     */
+    public function getImages(): array
+    {
+        return $this->images;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
 
 }
